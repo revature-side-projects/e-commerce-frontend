ac0f1146a3fe574953a796f6d2e36de1c01e57cd
k-n-terry <khenan.n.terry@gmail.com>
2022-05-18 19:44:55 -0400

    Consolidated code under dark-mode-switcher director

--- src/components/dark-mode-switcher/DarkModeSwitcher.tsx
+++ src/components/dark-mode-switcher/DarkModeSwitcher.tsx
@@ -1,19 +1,55 @@
-import Button from '@mui/material/Button'; // KHENAN TERRY: Import Button
 import React from "react";
+import { useState } from "react";
+import Button from '@mui/material/Button'; // KHENAN TERRY: Import Button
 import styled from "styled-components";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const lightMode = createTheme();
+lightMode.palette.primary.main = '#1976d2';
+lightMode.palette.background.default = '#ffffff';
+
+const darkMode = createTheme();
+darkMode.palette.primary.main = '#ef5350';
+lightMode.palette.background.default = '#000000';
 
 export default function DarkModeSwitcher(){
+  const [theme, setTheme] = useState(true);
+  const activeTheme = createTheme(theme ? lightMode : darkMode);
+
   return(
-      <>
-          <Button
-            color='primary'
-            variant='contained'
-            sx={{ mt: 0.30, mb: 0 }}
-            onClick={() => {sessionStorage.setItem('colorMode', sessionStorage.getItem('colorMode') === 'darkMode' ? 'lightMode' : 'darkMode')}}>
+    <>
+      <ThemeProvider theme={activeTheme}>
+        <Button
+          color='primary'
+          variant='contained'
+          sx={{ mt: 0.30, mb: 0 }}
+          onClick={() => setTheme(!theme)}>
 
-            Dark/Light Mode
-          </Button>
-      </>
+          Mode
+        </Button>
+      </ThemeProvider>
+    </>
   );
-
+  // const lightMode = createTheme();
+  // lightMode.palette.primary.main = '#ffffff';
+  // lightMode.palette.secondary.main = '#474C55';
+  //
+  // const darkMode = createTheme();
+  // darkMode.palette.primary.main = '#000000';
+  // darkMode.palette.secondary.main = '#474C55';
+  //
+  // const colorMode = sessionStorage.getItem('colorMode');
+  // const theme = colorMode ==='darkMode' ? darkMode : lightMode;
+  //
+  // return (
+  //   <ThemeProvider>
+  //   <Button>Primary</Button>
+  //     <Button color="secondary">Secondary</Button>
+  //     <CartContext.Provider value={value}>
+  //       <Router>
+  //         <AppRoutes></AppRoutes>
+  //       </Router>
+  //     </CartContext.Provider>
+  //   </ThemeProvider>
+  // );
 }
