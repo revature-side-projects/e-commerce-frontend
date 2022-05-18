574a4407458730b0391fd9008c4469da134429c3
CASEY GUNGEON <bound2theground@gmail.com>
2022-05-18 13:45:36 -0700

    fix local conflicts

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,18 @@ import ProductItem from './models/Product';
 import { AppRoutes } from './router/AppRoutes';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Button from '@mui/material/Button';
-import {red, blue} from '@mui/material/colors'
 
 function App() {
   const [cart, setCart] = useState<ProductItem[]>([]);
   const value = { cart, setCart };
 
   const lightMode = createTheme();
-  lightMode.palette.primary.main = '#dddddd';
-  lightMode.palette.secondary.main = '#ff00ff';
+  lightMode.palette.primary.main = '#ffffff';
+  lightMode.palette.secondary.main = '#474C55';
 
   const darkMode = createTheme();
   darkMode.palette.primary.main = '#000000';
-  darkMode.palette.secondary.main = '#00ffff';
+  darkMode.palette.secondary.main = '#474C55';
 
   const colorMode = sessionStorage.getItem('colorMode');
   const theme = colorMode ==='darkMode' ? darkMode : lightMode;
