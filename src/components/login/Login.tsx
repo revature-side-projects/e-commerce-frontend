7cb1c29cc61312fa7cc9587c20c3abbab20f13bb
tsimmons15 <tsimmons15@knights.ucf.edu>
2022-05-24 10:50:06 -0400

    Update Login.tsx
    Fixed an issue with a doubled up </Grid> on 85 and missing </ThemeProvider> on 88.

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -82,9 +82,9 @@ export default function Login() {
                 </Link>
               </Grid>
             </Grid>
-          </Grid>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </ThemeProvider>
   );
 }
