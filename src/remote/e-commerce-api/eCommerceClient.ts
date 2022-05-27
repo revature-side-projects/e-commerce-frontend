7636578b33a0306234ec34cfcf89c4ef2aa9c33a
tsimmons15 <tsimmons15@valenciacollege.edu>
2022-05-27 12:02:06 -0400

    switching away from EBS instance.

--- src/remote/e-commerce-api/eCommerceClient.ts
+++ src/remote/e-commerce-api/eCommerceClient.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 // Very simple, but it also doesn't give us much abstraction
 const eCommerceClient = axios.create({
   withCredentials: true,
-  baseURL: 'http://revatureswag-env.eba-3gm7fua7.us-east-1.elasticbeanstalk.com/',
+  baseURL: 'http://ec2-54-83-92-30.compute-1.amazonaws.com',
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': 'http://localhost:3000',
