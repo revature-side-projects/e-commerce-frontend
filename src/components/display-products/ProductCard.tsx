f5b86ede768702751cf8776e23676c356a1b573b
SosaTheHuster <arcane1241@gmail.com>
2022-07-14 16:08:48 -0400

    fixed some styling issues

--- src/components/display-products/ProductCard.tsx
+++ src/components/display-products/ProductCard.tsx
@@ -1,13 +1,13 @@
 import {
-    SearchOutlined,
-    ShoppingCartOutlined,
-  } from "@material-ui/icons";
+  SearchOutlined,
+  ShoppingCartOutlined,
+} from "@material-ui/icons";
 import { useContext } from "react";
-  import styled from "styled-components";
+import styled from "styled-components";
 import { CartContext } from "../../context/cart.context";
 import Product from "../../models/Product";
-  
-  const Info = styled.div`
+
+const Info = styled.div`
     opacity: 0;
     width: 100%;
     height: 100%;
@@ -22,8 +22,8 @@ import Product from "../../models/Product";
     transition: all 0.5s ease;
     cursor: pointer;
   `;
-  
-  const Container = styled.div`
+
+const Container = styled.div`
     flex: 1;
     margin: 5px;
     min-width: 280px;
@@ -37,21 +37,23 @@ import Product from "../../models/Product";
       opacity: 1;
     }
   `;
-  
-  const Circle = styled.div`
+
+const Circle = styled.div`
     width: 200px;
     height: 200px;
     border-radius: 50%;
     background-color: white;
     position: absolute;
   `;
-  
-  const Image = styled.img`
+
+const Image = styled.img`
+    max-width: 100%;
     height: 75%;
     z-index: 2;
   `;
-  
-  const Icon = styled.div`
+
+const Icon = styled.div`
+    color: black;
     width: 40px;
     height: 40px;
     border-radius: 50%;
@@ -66,40 +68,40 @@ import Product from "../../models/Product";
       transform: scale(1.1);
     }
   `;
-  
-  interface productProps {
-      product: Product,
-      key: number
+
+interface productProps {
+  product: Product,
+  key: number
+}
+
+export const ProductCard = (props: productProps) => {
+  const { cart, setCart } = useContext(CartContext);
+
+  const addItemToCart = (product: Product) => {
+
+    const newCart = [...cart]
+    const index = newCart.findIndex((searchProduct) => {
+      return searchProduct.id === product.id
+    })
+
+    if (index === -1) newCart.push(product)
+    else newCart[index].quantity += product.quantity
+
+    setCart(newCart)
   }
 
-  export const ProductCard = (props: productProps) => {
-    const { cart, setCart } = useContext(CartContext);
-
-    const addItemToCart = (product: Product) => {
-
-      const newCart = [...cart]
-      const index = newCart.findIndex((searchProduct) => {
-        return searchProduct.id === product.id
-      })
-
-      if (index === -1) newCart.push(product)
-      else newCart[index].quantity += product.quantity
-
-      setCart(newCart)
-    }
-
-    return (
-      <Container>
-        <Circle />
-        <Image src={props.product.image} />
-        <Info>
-          <Icon>
-            <ShoppingCartOutlined onClick={() => {addItemToCart({...props.product, quantity: 1})}} />
-          </Icon>
-          <Icon>
-            <SearchOutlined />
-          </Icon>
-        </Info>
-      </Container>
-    );
-  };
+  return (
+    <Container>
+      <Circle />
+      <Image src={props.product.image} />
+      <Info>
+        <Icon>
+          <ShoppingCartOutlined onClick={() => { addItemToCart({ ...props.product, quantity: 1 }) }} />
+        </Icon>
+        <Icon>
+          <SearchOutlined />
+        </Icon>
+      </Info>
+    </Container>
+  );
+};
