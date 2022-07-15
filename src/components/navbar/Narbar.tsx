664cd409512f90b605dbd66352378d42161ce65c
SosaTheHuster <arcane1241@gmail.com>
2022-07-15 12:16:42 -0400

    added documentation

--- src/components/navbar/Narbar.tsx
+++ src/components/navbar/Narbar.tsx
@@ -7,10 +7,12 @@ import styled from "styled-components";
 import Product from "../../models/Product";
 import { apiGetAllProducts } from "../../remote/e-commerce-api/productService";
 
+//Container Styling Componenet
 const Container = styled.div`
   height: 60px;
 `;
 
+//Wrapper Styling Component
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
@@ -18,15 +20,19 @@ const Wrapper = styled.div`
   justify-content: space-between;
 `;
 
+//Left Styling Component
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
 `;
 
+//Logo Styling Component
 const Logo = styled.h1`
   font-weight: bold;
 `;
+
+//Right Styling Component
 const Right = styled.div`
   flex: 1;
   display: flex;
@@ -34,6 +40,7 @@ const Right = styled.div`
   justify-content: flex-end;
 `;
 
+//MenuItem Styling Component
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
@@ -41,16 +48,19 @@ const MenuItem = styled.div`
 `;
 
 const Navbar = () => {
-  
+
+  //Navigate variable to useNavigate function.
   const navigate = useNavigate();
 
   return (
     <Container>
       <Wrapper>
         <Left>
+          {/*Left Side of Navbar*/}
           <Logo onClick={() => { navigate('/') }}>Revature Swag Shop</Logo>
         </Left>
         <Right>
+          {/*Right Side of Navbar*/}
           <MenuItem onClick={() => { navigate('/register') }}>REGISTER</MenuItem>
           <MenuItem onClick={() => { navigate('/login') }}>SIGN IN</MenuItem>
           <MenuItem onClick={() => { navigate('/cart') }}>
