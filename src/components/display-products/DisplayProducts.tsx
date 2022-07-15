a0c87b052c74aa854339015d79e80d09f2a51a32
SosaTheHuster <arcane1241@gmail.com>
2022-07-15 09:42:07 -0400

    Added in conditional rendering for filteredProducts, addeed in Search By Category

--- src/components/display-products/DisplayProducts.tsx
+++ src/components/display-products/DisplayProducts.tsx
@@ -1,4 +1,4 @@
-import { Input } from '@material-ui/core';
+import { FormControl, Input, InputLabel, MenuItem, Select } from '@material-ui/core';
 import React, { SyntheticEvent, useEffect, useState } from 'react';
 import styled from "styled-components";
 import Product from '../../models/Product';
@@ -29,16 +29,26 @@ const SearchBar = styled.input`
     padding: 10px 0px;
 `;
 
+const Text = styled.h1`
+    font-size: 30px;
+    font-weight: bold;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    text-align: center;
+    width: 100%;`
+
 export const DisplayProducts = () => {
 
   const [products, setProducts] = useState<Product[]>([]);
   const [filteredProducts, setFilteredProducts] = useState<Product[]>(products);
+  const [filterBy, setFilterBy] = useState("name");
+  const [category, setCategory] = useState("category");
 
   let search = (e: SyntheticEvent) => {
     let value = (e.target as HTMLInputElement).value;
 
     if (!value) {
-      setFilteredProducts(products);      
+      setFilteredProducts(products);
     } else {
       let results = products.filter((product: Product) => {
         return product.name.toLowerCase().includes(value.toLowerCase());
@@ -47,27 +57,72 @@ export const DisplayProducts = () => {
     }
 
   }
+
+  let categorySearch = () => {
+    if (category === "category") {
+      setFilteredProducts(products);
+    } else {
+      let results = products.filter((product: Product) => {
+        return product.category.toLowerCase().includes(category.toLowerCase());
+      })
+      if (results.length === 0) {
+        setFilteredProducts([]);
+      } else {
+        setFilteredProducts(results);
+      }
+    }
+  }
+
+  useEffect(() => {
+    if (filterBy === "category") {
+      categorySearch();
+    }
+  })
+
   useEffect(() => {
     const fetchData = async () => {
-      const result = await apiGetAllProducts();   
-      setProducts(result.payload);  
+      const result = await apiGetAllProducts();
+      setProducts(result.payload);
       setFilteredProducts(result.payload);
     }
     fetchData()
   }, [])
 
+
   return (
     <React.Fragment>
-        <Navbar/>
-        <SearchDiv>
-        <SearchBar type='text' onChange={search} placeholder="Search" className='searchbar'></SearchBar>
-        </SearchDiv>
-        <Container> 
-        {filteredProducts.map((item) => (
-            <ProductCard product={item} key={item.id} />
-        ))}
-        </Container>
+      <Navbar />
+      <SearchDiv>
+        {filterBy === "name" ? <SearchBar type='text' onChange={search} placeholder="Search" className='searchbar'></SearchBar> : null}
+        {filterBy === "category" ?
+          <Select
+            labelId="demo-simple-select-helper-label"
+            id="demo-simple-select-helper"
+            value={category}
+            label="Search"
+            onChange={event => setCategory(event.target.value as string)}            >
+            <MenuItem value="category">Category</MenuItem>
+
+            <MenuItem value="Moon">Moons</MenuItem>
+            <MenuItem value="Sun">Suns</MenuItem>
+            <MenuItem value="Star">Stars</MenuItem>
+          </Select> : null}
+        <Select
+          labelId="demo-simple-select-helper-label"
+          id="demo-simple-select-helper"
+          value={filterBy}
+          label="Search"
+          onChange={event => setFilterBy(event.target.value as string)}            >
+          <MenuItem value="name">Search By Name</MenuItem>
+          <MenuItem value="category">Search By Category</MenuItem>
+        </Select>
+      </SearchDiv>
+      <Container>
+        {filteredProducts.length <= 0 && <Text>No Products Found</Text>}
+        {filteredProducts.length > 0 && filteredProducts.map((item) => (
+          <ProductCard product={item} key={item.id} />))}
+      </Container>
     </React.Fragment>
-    
+
   );
 };
--- src/models/Product.ts
+++ src/models/Product.ts
@@ -5,13 +5,15 @@ export default class Product {
     price: number;
     description: string;
     image: string;
+    category: string;
 
-    constructor (id: number, name: string, quantity: number, description: string, price: number, image: string) {
+    constructor(id: number, name: string, quantity: number, description: string, price: number, image: string, category: string) {
         this.id = id;
         this.name = name;
         this.quantity = quantity;
         this.description = description;
         this.price = price;
         this.image = image;
+        this.category = category;
     }
 }
