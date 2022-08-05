ff986904e6cdddf9584a55b4f2a7b96c8b96c246
Matthew Coplan <mcoplanwork@gmail.com>
2022-08-05 12:09:34 -0400

    fixed issue with approutes displaying errior for EditProfile

--- src/router/AppRoutes.tsx
+++ src/router/AppRoutes.tsx
@@ -4,10 +4,10 @@ import { Cart } from '../components/cart/Cart';
 import Checkout from '../components/checkout/Checkout';
 import { DisplayProducts } from "../components/display-products/DisplayProducts";
 import Login from '../components/login/Login';
-import EditProfile from '../components/profile/EditProfile';
 import Register from '../components/register/Register';
 import UpdateProduct from '../components/modify-products/UpdateProduct';
 import { CreateProduct } from '../components/create-product/CreateProduct';
+import EditProfile from '../components/profile/EditProfile';
 
 export const AppRoutes: React.FC<unknown> = () => {
   const [user, setUser] = useState("");
