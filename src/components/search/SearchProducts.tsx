df33af0fa1045bf946eac0b362f510f2597dcabd
Caleb Kats <calebkats0@gmail.com>
2022-08-06 13:57:03 -0500

    idk

--- src/components/search/SearchProducts.tsx
+++ src/components/search/SearchProducts.tsx
@@ -1,7 +1,12 @@
-import React from "react"
-import {useLocation} from "react-router-dom";
+import React, { useEffect, useState } from "react"
+import {useLocation, useParams} from "react-router-dom";
 import Navbar from "../navbar/Narbar";
-import { Box } from "@mui/material";
+import { Box, Container, Grid, TextField, Typography } from "@mui/material";
+import { apiGetProductByName } from "../../remote/e-commerce-api/productService";
+import Product from "../../models/Product";
+import No_Image from "./No_Image.png"
+
+
 
 export const SearchProducts = () =>{
     const location = useLocation();
@@ -10,10 +15,16 @@ export const SearchProducts = () =>{
         <>
         <Navbar/>
         <Box sx={{marginTop: 8,display: 'flex',flexDirection: 'column',alignItems: 'center',}}>
-            <h1>
-                Test
-            </h1>
+            <Grid container xs={6}>
+            <img src="No_Image.png"></img>
+
             <p>{new URLSearchParams(location.search).get('keyword')}</p>
+            </Grid>
+            <Grid container xs={6}>
+                <Grid xs={12}>
+                    <h1> Product Name </h1>
+                </Grid>
+            </Grid>
         </Box>
         </>
 
